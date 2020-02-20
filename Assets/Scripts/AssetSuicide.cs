dab9708839bd8b9bb578ec7057ae29f70b28e547
Marvin Müller <marvin.mueller@code.berlin>
2020-02-20 01:55:50 +0100

    Make the tree disappear on contact with the player

---
+++ Assets/Scripts/AssetSuicide.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssetSuicide : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        print("Should poof");
+        GetComponent<Renderer>().enabled = false;
+    }
+}
