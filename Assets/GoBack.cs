edcabf218c711145f7fe3c08f36abce76cc3f7a4
Marvin Müller <marvin.mueller@code.berlin>
2020-03-12 16:35:00 +0100

    Make the axe return to it's default position when out of bounds

---
+++ Assets/GoBack.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoBack : MonoBehaviour
+{
+    public string tagName;
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "WorldBoundary")
+        {
+            this.transform.position = new Vector3(-0.78f, 0.442f, -0.6756546f);
+            this.transform.rotation = new Quaternion(0, 0, 43.152f, 0);
+        }
+    }
+}
