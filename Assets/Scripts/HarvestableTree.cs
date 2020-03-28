8d57a201c85206330f6714a2bbae7e00265198bd
Marvin Müller <marvin.mueller@code.berlin>
2020-03-28 12:04:21 +0100

    Add Sapling to the project and prepare for further changes

--- Assets/Scripts/HarvestableTree.cs
+++ Assets/Scripts/HarvestableTree.cs
@@ -6,6 +6,7 @@ public class HarvestableTree : MonoBehaviour
 {
     // Configuration
     public EnvironmentManager environmentManager;
+    public GameObject sapling;
     public GameObject stick;
     public int AmountOfSticksToSpawn;
     public float DropSquareSize;
@@ -47,6 +48,9 @@ public class HarvestableTree : MonoBehaviour
                     Quaternion.Euler(90f, Random.Range(90f, 360f), 0)); // configure the rotation it will spawn in
             }
             print("Sticks spawned.");
+
+            // Spawn sapling
+            Instantiate(this.sapling, this.transform.position, Quaternion.identity);
 
             // Call that the tree was removed to the EnvironmentManager
             environmentManager.CallTreeHarvested();
