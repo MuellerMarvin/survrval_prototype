cd70559987fc481da17834d2bdc7d45dbab352d8
Marvin Müller <marvin.mueller@code.berlin>
2020-04-14 10:18:15 +0200

    Rewrite whole ignitable script
    Can now be used for more than grass, can make the object darker, can use anything as fire, can use anything as an object to attach to, more configuration, can make object burn-out (disappear after some time),
    makes object reappear after some time as well

---
+++ Assets/Scripts/CustomEditors/IgnitableEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEditor.Experimental.UIElements.GraphView;
+
+[CustomEditor(typeof(Ignitable))]
+[CanEditMultipleObjects]
+public class IgnitableEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        // draw the default inspector configurations
+        base.OnInspectorGUI();
+
+        // cast the target script into a usable object
+        Ignitable myIgnitable = (Ignitable)target;
+
+        // add a slider for the blackLevel
+        myIgnitable.blackLevel = EditorGUILayout.Slider(myIgnitable.blackLevel, 0, 1, null);
+    }
+}
--- Assets/Scripts/Ignitable.cs
+++ Assets/Scripts/Ignitable.cs
@@ -1,165 +1,209 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class Ignitable : MonoBehaviour
 {
-    public GameObject fire;
-    public float currentHeat
-    {
-        get
-        {
-            return _currentHeat;
-        }
-        set
-        {
-            _currentHeat = value;
-        }
-    }
-    public float _currentHeat = 0;
-    public bool ignited
-    { get { return _ignited; }
-        set
-        {
-            _ignited = value;
-            this.fire.SetActive(_ignited);
-        }
-    }
-    public bool _ignited = false;
-    private float minHeat = 0;
-    private float spreadHeat = 100;
-    private float fullyBlackHeat = 2f;
-    private float destroyHeat = 1.5f;
+    // fire and meshObject
+    public GameObject fireObject;
+    public GameObject meshObject;
+
+    // Public variables and properties
+    public bool drawScanDistance = false;
+    public float scanForOthersDistance = 2f;
+    public bool ignited = false;
+    public float currentHeat = 0f;
+    public float changePerSecond = 0.05f;
+    public float spreadProbabilityPerFrame = 0.05f;
+    public float spreadHeat = 1f;
+    public float fullyBlackHeat = 2f;
+    public float destroyHeat = 3f;
+    public Color burntColor;
+    public float blackLevel { get; set; }
+
+    // Internal Variables
     private bool destroyed = false;
-    private float destroyedSince = 0;
-    private float respawnTime = 100;
-    public float heatIncreasePerFrame = 0.0005f;
-    public float spreadProbabilityPerFrame = 0.001f;
-    private List<GameObject> ignitableObjects = new List<GameObject>();
-    private Renderer grasRenderer;
-    private Color startingColor;
-    public float blackLevel
+    private Color defaultMaterialColor;
+    private List<GameObject> ignitableObjects;
+
+    /// <summary>
+    /// This function gets called once at the start of the game
+    /// </summary>
+    private void Start()
     {
-        get
-        {
-            return _blackLevel;
-        }
-        set
-        {
-            _blackLevel = value;
-        }
-    }
-   private float _blackLevel = 0;
+        // Scan for other Ignitable Objects
+        ScanForIgnitables();
 
-    void Start()
-    {
-        Collider[] hitColliders = Physics.OverlapSphere(transform.position, 0.1f); // list of all objects near this one
+        // save default color for black-level adjustment
+        defaultMaterialColor = this.meshObject.GetComponent<MeshRenderer>().material.color;
 
-        for (int i = 0; i < hitColliders.Length; i++)
-        {
-            if (hitColliders[i].tag == "Ignitable") // filter for ignitable objects
-            {
-                ignitableObjects.Add(hitColliders[i].gameObject);
-            }
-        }
-        foreach(Transform child in this.transform)
-        {
-            if(child.name == "Fire")
-            {
-                this.fire = child.gameObject;
-            }
-            else
-            {
-                this.grasRenderer = child.gameObject.GetComponent<Renderer>();
-            }
-        }
-        // get default values
-        startingColor = grasRenderer.material.GetColor("_Color");
-        print(this.fire);
-        currentHeat = 0;
-
-        // add some time to the respawntime to make it seem more natural
-        respawnTime += Random.Range(0, 100);
+        // first-time calculate blackLevel and ajust it
+        AdjustBlackLevel(CalculateBlackLevel());
     }
 
-    void Update()
+    /// <summary>
+    /// This function gets called on every rendered frame
+    /// </summary>
+    private void Update()
     {
-        if (destroyed)
+        AdjustBlackLevel(blackLevel);
+        if(ignited)
         {
-            this.gameObject.SetActive(false);
-            this.fire.SetActive(false);
-            destroyedSince += Time.deltaTime;
-            if(destroyedSince > respawnTime)
-            {
-                // reset the object
-                this.gameObject.SetActive(true);
-                ignited = false;
-                currentHeat = 0;
-            }
-        }
-        else if(ignited)
-        {
-            this.fire.SetActive(true);
-            currentHeat += heatIncreasePerFrame;
+            ShowFire(true);
+            currentHeat += changePerSecond * Time.deltaTime;
+
             if(currentHeat > destroyHeat)
             {
-                destroyed = true;
-                this.gameObject.SetActive(false);
+                ShowSelf(false);
+                ShowFire(false);
                 ignited = false;
-                return;
+                destroyed = true;
             }
-            // else
-            RecalculateBlackLevel();
-        }
-    }
-
-    private void UpdateColor()
-    {
-        grasRenderer.material.SetColor("_Color", new Color(startingColor.r * (1- blackLevel), startingColor.g * (1 - blackLevel), startingColor.b * (1 - blackLevel)));
-    }
-
-    private void RecalculateBlackLevel()
-    {
-        if (currentHeat > fullyBlackHeat)
-        {
-            blackLevel = 1.0f;
+            else if(currentHeat > spreadHeat)
+            {
+                Spread(spreadProbabilityPerFrame);
+            }
         }
         else
         {
-            blackLevel = ((fullyBlackHeat - minHeat) / 100) * currentHeat;
+            ShowFire(false);
+            // check if the current heat is below 0 - lock it to 0 as a minimum
+            if(currentHeat <= 0)
+            {
+                currentHeat = 0;
+            }
+            else
+            {
+                currentHeat -= changePerSecond * Time.deltaTime;
+            }
         }
-        UpdateColor();
+
+        // check if it's destroyed and if it's below 0 heat, show the object again
+        if (destroyed && currentHeat == 0)
+        {
+            ShowSelf(true);
+            ShowFire(false);
+        }
+
+        AdjustBlackLevel(CalculateBlackLevel()); // recalculate the blackLevel and adjust the color of the object to it
     }
 
-    private void Spread()
+    /// <summary>
+    /// This function will ignite the object, if it isn't destroyed - this should be the only way the object gets ignited at runtime
+    /// </summary>
+    /// <returns></returns>
+    public bool AttemptIgnite()
     {
-        if(ignitableObjects.Count > 0)
+        if(destroyed)
         {
-            print("spreading.");
-            int chosenOne = Random.Range(0, ignitableObjects.Count); // choose which one to ignite
-            ignitableObjects[chosenOne].GetComponent(this.GetType()).SendMessage("Ignite"); // ignite it
+            return false;
+        }
+        else
+        {
+            return ignited = true;
         }
     }
 
-    private bool Ignite()
+    /// <summary>
+    /// Spread to one object in range
+    /// </summary>
+    /// <returns>If it was spread to another object</returns>
+    public bool Spread()
     {
-        this.ignited = true;
-        return this.ignited;
+        Spread(1f);
+        return true;
     }
 
-    private bool Extinguish()
+    /// <summary>
+    /// Spread to one object in range, with a certain probability
+    /// </summary>
+    /// <param name="probability">probability of spread from 0 to 1</param>
+    /// <returns>If the fire was spread</returns>
+    public bool Spread(float probability)
     {
-        this.ignited = false;
-        currentHeat = 0;
-        return this.ignited;
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if(ignited && other.tag == "Axe")
+        if(ignitableObjects.Count == 0)
         {
-            Extinguish(); // extinguish on contact with the axe
-            print("Extinguish");
+            print("Nowhere to spread to.");
+        }
+
+        if(Random.Range(0f, 1f) <= probability)
+        {
+            // Spread to a random neighbour
+            ignitableObjects[Random.Range(0, ignitableObjects.Count - 1)].SendMessage("AttemptIgnite");
+        }
+        return false;
+    }
+
+    /// <summary>
+    /// Shows or hides the fire.
+    /// </summary>
+    /// <param name="fireVisible">Visibility of the fire.</param>
+    public void ShowFire(bool fireVisible)
+    {
+        this.fireObject.SetActive(fireVisible);
+    }
+
+    /// <summary>
+    /// Shows or hides the entire GameObject.
+    /// </summary>
+    /// <param name="selfVisible">Visibility of the GameObject.</param>
+    public void ShowSelf(bool selfVisible)
+    {
+        this.meshObject.SetActive(selfVisible);
+    }
+
+    /// <summary>
+    /// Changes the amount of black applied to the GameObject's material.
+    /// </summary>
+    /// <param name="blackLevel"></param>
+    private void AdjustBlackLevel(float blackLevel)
+    {
+        this.meshObject.GetComponent<MeshRenderer>().material.color = (defaultMaterialColor * (1 - blackLevel)) + (burntColor * blackLevel);
+    }
+
+    /// <summary>
+    /// Recalculates the blackLevel variable, where the minimum is 0 and the maximum is 1.
+    /// </summary>
+    /// <returns>blackLevel</returns>
+    private float CalculateBlackLevel()
+    {
+        if(currentHeat == 0)
+        {
+            blackLevel = 0;
+        }
+        else if(currentHeat >= fullyBlackHeat)
+        {
+            blackLevel = 1;
+        }
+        else
+        {
+            blackLevel = currentHeat / fullyBlackHeat;
+        }
+
+        return blackLevel;
+    }
+
+    private void ScanForIgnitables()
+    {
+        // Create a new empty list
+        ignitableObjects = new List<GameObject>();
+
+        // fill the list with all ignitable objects in 'scanForOthersDistance' distance
+        foreach(Collider collider in Physics.OverlapSphere(this.gameObject.transform.position, scanForOthersDistance))
+        {
+            if (collider.gameObject.tag.ToLower() == "ignitable")
+            {
+                ignitableObjects.Add(collider.gameObject);
+            }
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        // Draw a red sphere the size of the configured scan-distance, if the option is enables
+        if(drawScanDistance)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(transform.position, scanForOthersDistance);
         }
     }
 }
