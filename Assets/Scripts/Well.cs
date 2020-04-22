67a5ee7a472885f15bd4b0d11564a03eb9e2ab46
Marvin Müller <marvin.mueller@code.berlin>
2020-04-22 23:04:32 +0200

    Full well implementation

--- Assets/Scripts/Well.cs
+++ Assets/Scripts/Well.cs
@@ -5,103 +5,126 @@ using Valve.VR.InteractionSystem;
 
 public class Well : MonoBehaviour
 {
-    // Game objects and transforms
-    public GameObject handle;
-    public GameObject bucket;
-    public Transform bucketDown;
+    public CircularDrive handleDrive;
     public Transform bucketUp;
+    public Transform bucketDown;
 
-    // Other configurations
-    public float maxHandleRotations;
-
-    #region Properties
-    // External Scripts
-    private Fillable bucketScript {
+    private GameObject bucket;
+    public bool isBucketAttached {
         get
         {
-            return (Fillable)bucket.GetComponent<Fillable>();
+            return _isBucketAttached;
         }
-    }
-    private HandleTracker handleTracker
-    {
-        get
-        {
-            return (HandleTracker)handle.GetComponent<HandleTracker>();
-        }
-    }
-
-    // Other Propertie
-    public bool bucketAttached
-    {
-        get { return _bucketAttached; }
         set
         {
-            foreach(MeshRenderer childRenderer in bucket.GetComponentsInChildren<MeshRenderer>())
+            handleDrive.enabled = value;
+            _isBucketAttached = value;
+        }
+    }
+    public bool isBucketUp
+    {
+        get
+        {
+            if(bucketHeight > 0.9)
             {
-                childRenderer.enabled = value;
+                return true;
             }
-            _bucketAttached = value;
+            return false;
         }
     }
-    public float attachedBucketFillAmount
+    public bool isBucketDown
     {
         get
         {
-            return bucketScript.currentFillAmount;
-        }
-        set
-        {
-            bucketScript.currentFillAmount = value;
-        }
-    }
-    public float attachedBucketMaxFillAmount
-    {
-        get
-        {
-            return bucketScript.maxFillAmount;
-        }
-        set
-        {
-            bucketScript.maxFillAmount = value;
+            if(bucketHeight < 0.1)
+            {
+                return true;
+            }
+            return false;
         }
     }
     public float bucketHeight
     {
         get
         {
-            return _bucketHeight;
-        }
-        set
-        {
-            print("Valueee:" + value);
-            _bucketHeight = Mathf.Clamp01(value);
-            bucket.gameObject.transform.localPosition = bucketDown.localPosition + (bucketUp.localPosition * _bucketHeight);
+            return ((handleDrive.outAngle - handleDrive.minAngle) / (handleDrive.maxAngle - handleDrive.minAngle));
         }
     }
-    public float currentHandleRotations
+    public bool isBucketGrabbable
     {
         get
         {
-            print(handle.transform.localEulerAngles);
-            return 0f;
+            if(isBucketAttached)
+            {
+                return _isBucketGrabbable;
+            }
+            return false;
         }
-        set { }
+        set
+        {
+            if(isBucketAttached)
+            {
+                bucket.GetComponent<Throwable>().enabled = value;
+                bucket.GetComponent<Interactable>().enabled = value;
+                bucket.GetComponent<Rigidbody>().freezeRotation = !value;
+            }
+        }
+    }
+    private bool _isBucketGrabbable;
+    private bool _isBucketAttached;
+
+    private void Start()
+    {
+        isBucketAttached = false;
     }
 
-    // Properties Storage
-    private bool _bucketAttached;
-    private float _bucketHeight;
-    #endregion
-
-
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// Runs each frame
+    /// </summary>
+    private void Update()
     {
+        // Update the bucket's position, to keep it in one place
+        UpdateBucket();
+
+        if(isBucketDown && isBucketAttached)
+        {
+            bucket.GetComponent<Fillable>().Fill(float.MaxValue);
+        }
+
+        if(isBucketUp && isBucketAttached)
+        {
+            // make it available to grab
+            isBucketGrabbable = true;
+        }
+
+        if(!isBucketUp && isBucketAttached)
+        {
+            // make it unavailable to grab
+            isBucketGrabbable = false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public GameObject DetachBucket()
     {
-        bucketHeight = (handleTracker.rotationsMade / maxHandleRotations);
+        this.isBucketAttached = false;
+        GameObject returnBucket = this.bucket;
+        this.bucket = null;
+        return returnBucket;
+    }
+
+    public void AttachBucket(GameObject bucket)
+    {
+        this.bucket = bucket;
+        this.isBucketAttached = true;
+    }
+
+    void UpdateBucket()
+    {
+        if (!isBucketAttached) return; // if the bucket isn't attached, don't run this function
+
+        // Position the bucket
+        print(bucketHeight);
+        bucket.transform.position = (1 - bucketHeight) * bucketDown.position + bucketUp.position * bucketHeight;
+        bucket.gameObject.transform.rotation = new Quaternion(0, 0, 0, 0);
     }
 }
